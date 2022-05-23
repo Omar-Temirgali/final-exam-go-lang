77fd08086ceb138f3189bc6fca2bf64dc9d3f6e4
Omar-Temirgali <190103073@stu.sdu.edu.kz>
2022-05-23 15:45:20 +0600

    Adding models

--- domain/model/article.go
+++ domain/model/article.go
@@ -1,7 +1,9 @@
 package model
 
 type Article struct {
-	ID      int
-	Title   string
-	Content string
+	ID      uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	Title   string `gorm:"type:varchar(255)" json:"title"`
+	Content string `gorm:"type:text" json:"description"`
+	UserID  uint64 `gorm:"not null" json:"-"`
+	User    User   `gorm:"foreignkey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
 }
--- domain/model/user.go
+++ domain/model/user.go
@@ -1,7 +1,9 @@
 package model
 
 type User struct {
-	ID        int
-	FirstName string
-	LastName  string
+	ID       uint64 `gorm:"primary_key:auto_incrememnt" json:"id"`
+	Name     string `gorm:"type:varchar(255)" json:"name"`
+	Email    string `gorm:"uniqueIndex;type:varchar(255)" json:"email"`
+	Password string `gorm:"->;<-;not null" json:"-"`
+	Token    string `gorm:"-" json:"token,omitempty"`
 }
